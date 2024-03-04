798bc18fae57acf1d255a3b65bc501dc6d57ae19
Capucine Darteil <capucinedarteil@gmail.com>
2024-03-04 17:56:21 +0100

    Beginning if the baseline

---
+++ baseline/preprocess.py
@@ -0,0 +1,17 @@
+import os
+SAMPLE_SIZE = os.environ.get('SAMPLE_SIZE',0.5)
+
+# Labels between 1 (malign) and 0 (benign)
+def labelize(df):
+    benign_classes = [4,2,3]
+    for i in range(1,10016):
+        if df['label'][i-1] in benign_classes:
+            df ['label'][i-1] = 0
+        else:
+            df['label'][i-1] = 1
+    return df
+
+#Splits into a SAMPLE_SIZE% sample
+def sampler(df):
+    df_sample = df.sample(SAMPLE_SIZE*df.shape[0], random_state = 42)
+    return df_sample
