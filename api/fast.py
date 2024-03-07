2243c06fd58e384dd3c999360147ba4a0ab76efd
Pavel Bautista <pavelbautista@gmail.com>
2024-03-07 17:38:41 +0100

    progress in fast.py + new requirements

--- api/fast.py
+++ api/fast.py
@@ -1,12 +1,31 @@
 import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from Skin_Project.ml_logic.registry import load_model
+from PIL import Image
 
 app = FastAPI()
+
+# Allowing all middleware is optional, but good practice for dev purposes
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+# app.state.model = load_model()
 
 @app.get("/")
 def root():
     return {
     'greeting': 'Hello'
 }
+
+@app.post("/image")
+def image_process(path):
+    #récupérer chemin de la photo
+    #preprocess l'image
+    #model.predict image (besoin d'avoir un load model fonctionnel)
+    pass
