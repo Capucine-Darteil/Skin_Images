4c86d0e053da4d06ce1ee100d972480fe5036996
Pavel Bautista <pavelbautista@gmail.com>
2024-03-08 16:34:09 +0100

    progress in api fast.py (predict) + add params

--- api/fast.py
+++ api/fast.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from Skin_Project.ml_logic.registry import load_model
@@ -6,6 +7,7 @@ from PIL import Image
 import os
 from Skin_Project.ml_logic.data import get_data, resize_data, flat_images
 from Skin_Project.params import *
+from Skin_Project.ml_logic.registry import load_best_model
 
 app = FastAPI()
 
@@ -30,10 +32,32 @@ def root():
 def image_process(CHEMIN_TEST,gender,age,location):
     image = get_data(CHEMIN_TEST)
     image_resized = resize_data(image,int(IMAGE_SIZE))
+    new_image_processed = flat_images(image_resized)
+    new_image_processed = new_image_processed.reset_index()
+    new_image_processed = new_image_processed.drop(columns='index')
     metadata_dict = {'Age':age,'Sex':gender, 'Localization':location}
-    image_metadata = pd.DataFrame(metadata_dict)
-    new_image_processed = flat_images()
-    #récupérer chemin de la photo
-    #preprocess l'image
-    #model.predict image (besoin d'avoir un load model fonctionnel)
-    pass
+    image_metadata = pd.DataFrame(metadata_dict,index=[0])
+    df_new_image = pd.concat([image_metadata,new_image_processed],axis=1)
+
+    return df_new_image
+
+def binary_classification_predict(df_new_image,THRESHOLD):
+    model = load_best_model()
+    threshold = THRESHOLD
+    df_new_image = df_new_image.drop(columns=['Age', 'Sex', 'Localization'])
+    df_new_image = df_new_image/255
+    df_new_image = np.array(df_new_image).reshape(len(df_new_image), IMAGE_SIZE, IMAGE_SIZE, 3)
+    prediction = model.predict(df_new_image)
+    print(prediction)
+    if prediction[0][0] < float(threshold) :
+        return 0
+    else:
+        return 1
+
+def all_classification_predict(df_new_image):
+    df_new_image = df_new_image.drop(columns=['Age', 'Sex', 'Localization'])
+    df_new_image = df_new_image/255
+    df_new_image = np.array(df_new_image).reshape(len(df_new_image), IMAGE_SIZE, IMAGE_SIZE, 3)
+    prediction = model.predict(df_new_image)
+    print(prediction)
+    return prediction
