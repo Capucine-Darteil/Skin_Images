25859a9384ca421a40f4b8d09f0d29e52b37e96e
Pavel Bautista <pavelbautista@gmail.com>
2024-03-13 11:51:40 +0100

    I gave up on third model with metadata, sorry guys

--- app/app.py
+++ app/app.py
@@ -5,13 +5,34 @@ from PIL import Image
 from Skin_Project.params import *
 import numpy as np
 from io import StringIO
+import json
 
-st.title(':violet[Skin cancer] detector')
+st.markdown("<h1 style='text-align: center; color: violet;'>Mole type detector</h1>", unsafe_allow_html=True)
 
-st.markdown("### Please upload your mole photo")
-img_file_buffer = st.file_uploader('Upload an image')
+st.markdown("""<style>.subheader {font-size:20px !important;}</style>""", unsafe_allow_html=True)
+st.markdown('<p style="text-align:center;"class="subheader", >Disclaimer : this tool is not intended to replace the expertise of a doctor</p>', unsafe_allow_html=True)
+
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: skyblue;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown("""<style>.subheader_2 {font-size:15px !important;}</style>""", unsafe_allow_html=True)
+st.markdown('<p style="class="subheader_2", >Please upload your mole photo</p>', unsafe_allow_html=True)
+img_file_buffer = st.file_uploader('Upload an image',type=["png", "jpg", "jpeg"])
 
 if img_file_buffer is not None :
+    # img = Image.open(img_file_buffer)
+    # st.markdown(f'<div style="text-align:center"><img src="data:image/png;base64,{img_file_buffer}" alt="Uploaded Image" style="width: 50%;"></div>',
+    # unsafe_allow_html=True)
+    # st.write("Here's the image you uploaded ☝️")
+
     st.image(Image.open(img_file_buffer), caption="Here's the image you uploaded ☝️")
 
 if img_file_buffer is not None:
@@ -47,28 +68,51 @@ if img_file_buffer is not None:
 
 
 
+
+# with open('data.json', 'w') as fp:
+#     json.dump(data, fp)
+
+# with open('data.json', 'rb') as fp:
+#     json_data = json.load(fp)
+# st.write(json_data)
+# st.write(type(json_data))
+
+
 sex = st.selectbox('Your sex:',('Male', 'Female'))
 age = st.number_input('Your age:', key=int,min_value=1, max_value=99)
 localization = st.selectbox('The localization of the mole:', ('Lower extremity', 'Trunk', 'Upper extremity', 'Scalp', 'Abdomen', 'Ear', 'Back', 'Face','Chest','Foot','Neck','Scalp','Hand','Genital','Acral'))
 
 
-
-data = {
-    "sex":sex,
-    "age":age,
-    "localization":localization
-}
-
-if st.button('Predict with metadata'):
-    with st.spinner("Wait for it..."):
+if st.button("Analyze Mole"):
+    if img_file_buffer is not None:
         img_bytes = img_file_buffer.getvalue()
-        res = requests.post(API_URL + "/predict_metadata", files={'img': img_bytes},data=data)
-        st.write(res)
-        if res.status_code == 200:
-            ### Display the image returned by the API
-            final = res.content
-            st.markdown(final)
-                    # st.markdown(final.decode('utf-8'))
+        data = {"sex": sex, "age": int(age), "localization": localization}
+        # Send image and data to API
+        files = {'image': img_file_buffer}
+        response = requests.post(API_URL + "/predict_metadata", files={'image': img_bytes},data={'data':json.dumps(data)})
+
+        # Handle response
+        if response.status_code == 200:
+            processed_data = response.json().get('processed_data', 'No data found')
+            st.write(processed_data)
         else:
-            st.markdown("**Oops**, something went wrong 😓 Please try again.")
-            st.write(res.status_code, res.content)
+            st.error("An error occurred during metadata analysis.")
+
+
+
+# if img_file_buffer is not None:
+#     if st.button('Predict with metadata'):
+#         with st.spinner("Wait for it..."):
+#             img_bytes = img_file_buffer.getvalue()
+#             st.write("yoooooo")
+#             res = requests.post(API_URL + "/predict_metadata", data=data, files={'img': img_bytes})
+
+#             st.write(res)
+#             if res.status_code == 200:
+#                 ### Display the image returned by the API
+#                 final = res.content
+#                 st.markdown(final)
+#                         # st.markdown(final.decode('utf-8'))
+#             else:
+#                 st.markdown("**Oops**, something went wrong 😓 Please try again.")
+#                 st.write(res.status_code, res.content)
